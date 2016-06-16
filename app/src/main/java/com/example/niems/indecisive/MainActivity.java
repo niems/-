f980b7aeb80c032efea97178a853d97bd122dd87
niems <czniemann1990@gmail.com>
2016-06-16 17:25:27 -0500

    added listview which is populated by the arraylist of options.

--- app/src/main/java/com/example/niems/indecisive/MainActivity.java
+++ app/src/main/java/com/example/niems/indecisive/MainActivity.java
@@ -1,23 +1,27 @@
 package com.example.niems.indecisive;
 
 import android.app.Dialog;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static ArrayList<Button> all_choices = new ArrayList<>(); //holds all user options
+    //public static ArrayList<Button> all_choices = new ArrayList<>(); //holds all user options
+    public static ArrayList<String> all_choices_string = new ArrayList(); //holds all user options as strings
     private View.OnClickListener button_listener;
     private Dialog add_option_dialog; //used to read the option in from the user
     private static boolean first_run = false; //true after the first execution
@@ -27,16 +31,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView( R.layout.activity_main);
 
         try{
-            /*
-            if(!first_run){
-                this.button_listener = new View.OnClickListener(){
-                    public void onClick(View v){
-                        addOption(v);
-                    }
-                };
 
-                first_run = true;
-            } */
+            //new listview stuff
+            String [] list_test = {"test 1", "test 2", "last test" };
+            ArrayAdapter<String> list_adapter = new ArrayAdapter<String>(this, R.layout.option_listview_main, all_choices_string);
+            ListView options_view = (ListView) findViewById( R.id.option_list );
+            options_view.setAdapter( list_adapter );
+
 
             Toolbar toolbar = (Toolbar) findViewById( R.id.toolbar_main );
             setSupportActionBar( toolbar );
@@ -67,22 +68,11 @@ public class MainActivity extends AppCompatActivity {
     public void dialogConfirm( View view ){
 
         try{
-            LinearLayout layout = (LinearLayout) findViewById( R.id.layout_main );
+            //LinearLayout layout = (LinearLayout) findViewById( R.id.layout_main );
             EditText text = (EditText) this.add_option_dialog.findViewById( R.id.add_option_dialog );
 
-            Button new_option = new Button(this);
-            String option_text = text.getText().toString();
-
-            new_option.setId( View.generateViewId() );
-            new_option.setLayoutParams( new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT ) );
-            new_option.setText( option_text );
-            new_option.setTextColor( getResources().getColor( R.color.colorPrimaryText ) );
-            new_option.setTextSize(18);
-            new_option.setBackgroundResource( R.drawable.current_options_main );
-            new_option.setOnClickListener(this.button_listener);
-
-            all_choices.add( new_option );
-            layout.addView( new_option );
+            String option_text = text.getText().toString().toUpperCase();
+            all_choices_string.add( option_text );
 
             this.add_option_dialog.dismiss();
 
@@ -105,6 +95,40 @@ public class MainActivity extends AppCompatActivity {
 
         }catch(Exception e){
             Toast.makeText(this, "Error: MainActivity - addOption()", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void decision( View view ){
+        try{
+            int min = 0;
+            int max = all_choices_string.size() - 1;
+
+            if(max >= 0 ){
+                int selection = min + (int)( Math.random() * ((max - min) + 1) );
+
+                Toast toast = Toast.makeText( getApplicationContext(), all_choices_string.get(selection), Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+
+                //Toast.makeText(this, all_choices_string.get( selection ), Toast.LENGTH_LONG).show();
+
+                /*
+                Toast decision = new Toast( getApplicationContext() );
+
+
+                decision.setText( all_choices_string.get( selection ) );
+                decision.setGravity(Gravity.CENTER, 0, 0);
+
+                decision.show();
+                */
+            }
+
+            else{
+                Toast.makeText(this, "Do not ask me to decide when you have not decided yourself", Toast.LENGTH_LONG).show();
+            }
+
+        }catch(Exception e){
+            Toast.makeText(this, "Error: MainActivity - decision()", Toast.LENGTH_SHORT).show();
         }
     }
 }
